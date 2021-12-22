e1f9fc65dae8a53bf81439cac79d7c8b3e501d2c
Wilder Trujillo <nadie437@gmail.com>
2021-12-22 05:13:27 -0500

    agregando variables de entorno

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Home pge')
@@ -13,4 +15,6 @@ app.get('*', (req, res) => {
   res.send('Page not found')
 })
 
-app.listen(8080)
+app.listen(port, ()=> {
+  console.log(`listening on http://localhost:${port}`);
+})
