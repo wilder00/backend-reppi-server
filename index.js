239b604237421dd992fe59ad5b1ad78100a9a115
Wilder Trujillo <nadie437@gmail.com>
2021-12-22 04:26:47 -0500

    enviando un attachment csv

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-  res.writeHead(200,{'content-type':'text/plain'})
-  const persona = {
-    id: 1,
-    nombre: 'Fernando '
-  }
-  res.write(JSON.stringify(persona))
+  res.setHeader('Content-Disposition', 'attachment; filename=list.csv')
+  res.writeHead(200,{'content-type':'application/csv'})
+  
+  res.write('id,nombre\n')
+  res.write('1,Maria\n')
+  res.write('2,Renata\n')
+  res.write('3,Fernanda\n')
   res.end()
 })
 
