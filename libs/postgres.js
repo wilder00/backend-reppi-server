12894daf490025316dda4316916ef40923a9233b
Wilder Trujillo <nadie437@gmail.com>
2021-12-23 21:28:00 -0500

    agregando la variable y ssl configuración para heroku

--- libs/postgres.js
+++ libs/postgres.js
@@ -3,11 +3,18 @@ const {Pool} = require('pg')
 const { environment } = require('../config/config')
 const USER  = encodeURIComponent(environment.dbUser)
 const PASSWORD = encodeURIComponent(environment.dbPassword)
-const URI = `postgres://${USER}:${PASSWORD}@${environment.dbHost}:${ environment.dbPort}/${environment.dbName}`
+//const URI = `postgres://${USER}:${PASSWORD}@${environment.dbHost}:${ environment.dbPort}/${environment.dbName}`
+const URI = environment.dbURI 
 
-const pool = new Pool({
-  connectionString : URI
-})
+const options = {
+  connectionString : URI,
+}
+if(environment.isProduction){
+  options.ssl = {
+    rejectUnauthorized: false,
+  }
+}
+const pool = new Pool(options)
 
 
 pool.on('error', (err, client) => {
